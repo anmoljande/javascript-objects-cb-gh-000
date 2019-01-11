e17f2238c1cec8b526c6727b302c8c6821918b97
Anmol Jande <jandanmol@gmail.com>
2019-01-11 17:25:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ var playlist=
 
 function updatePlaylist(playlist,artist,song)
 {
-  playlist[artist]=value;
+  playlist[artist]=song;
   return playlist;
 }
 function removeFromPlaylist(playlist,artist)
